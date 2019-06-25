76fdb2fd9da982528e56c8ed4580166a2261b5ae
Samuel <22435185+samueldlay@users.noreply.github.com>
2019-06-25 00:05:07 -0500

    Add string parsing module

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 'use strict';
 
-const env = require(__dirname + '/private/env.json');
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const isValidString = require(__dirname + '/is-valid-string.js');
+// const env = require(__dirname + '/private/env.json');
+// const Discord = require('discord.js');
+// const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+// client.on('ready', () => {
+//   console.log(`Logged in as ${client.user.tag}!`);
+// });
 
-client.on('message', msg => {
-  if (msg.content === 'no u') {
-    msg.reply('no u');
-  }
-});
+// client.on('message', msg => {
+//   if (isValidString(msg.content)) {
+//     msg.reply('no u');
+//   }
+// });
 
-client.login(env.bot.token);
+// client.login(env.bot.token);
+
+const msg = 'no u';
+const valid = isValidString(msg);
+console.log(valid);
---
+++ is-valid-string.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = function isValidString (string) {
+  if (string === 'no u') {
+    return true;
+  }
+  else return false;
+};
