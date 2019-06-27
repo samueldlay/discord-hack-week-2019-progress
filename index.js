1759f8ba12fc4a0ffce6e88c05361e409cc049c4
Samuel <22435185+samueldlay@users.noreply.github.com>
2019-06-26 19:34:58 -0500

    halloween extravaganza

--- index.js
+++ index.js
@@ -9,9 +9,28 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'no u') {
-    msg.reply('no u');
+  const string = 'halloween';
+  const string2 = 'test';
+  const string3 = 'nice';
+  const happyHallow = new Discord.RichEmbed;
+  if (msg.content.toUpperCase().includes(string.toUpperCase())) {
+    // msg.reply('God is his only son');
+    
+    return msg.reply(happyHallow.addField('First Favorite Candybar:', 'Snickers', false).addField('Second Favorite Candybar:', 'PayDay', false).setColor('ORANGE').setTitle('Happy Halloween!').setImage('https://i.pinimg.com/originals/34/c8/e4/34c8e48e1821ccf73aace421fc1b5cd1.gif'));
   }
+
+  if (msg.content.toUpperCase() === string2.toUpperCase()) {
+    // msg.reply('God is his only son');
+    happyHallow.author = 'Samuel';
+    return msg.reply(`The Author: ${happyHallow.author}`);
+  }
+  
+  if (msg.content.toUpperCase().includes(string3.toUpperCase())) {
+    // msg.reply('God is his only son');
+    
+    return msg.reply(happyHallow.setColor('PURPLE').setTitle('Fuck Yo Couch!').setImage('https://memegenerator.net/img/instances/64102014.jpg'));
+  }
+  
 });
 
 client.login(env.bot.token);
